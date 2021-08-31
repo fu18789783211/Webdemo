0afe5d7989a97e8ce08ad72e42846e6e8addf00a
fu18789783211 <1336177628@qq.com>
2021-08-31 22:21:59 +0800

    8/31

---
+++ Relaxer/script.js
@@ -0,0 +1,13 @@
+const circle=document.getElementById('Circle');
+const text=document.getElementById('text');
+
+
+// 总时间
+var totaltime=7500;
+var holdtime=1500;
+var zoomtime=6000;
+
+// 动画缩放
+function animationzoom(){
+    
+}
